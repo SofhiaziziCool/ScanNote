42e9f05634c2fbcc4ba6f2c18125251ef458e5af
cobboldGit <augustinecobbold6@gmail.com>
2023-12-31 20:01:36 +0000

    ml error

---
+++ app/src/main/java/com/example/scannote/SignupActivity.java
@@ -0,0 +1,6 @@
+package com.example.scannote;
+
+import android.app.Activity;
+
+public class SignupActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/scannote/database/entity/User.java
@@ -0,0 +1,51 @@
+package com.example.scannote.database.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user_table")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    int id;
+
+    @ColumnInfo(name = "email_column")
+    String email;
+
+    @ColumnInfo(name = "password_column")
+    String password;
+
+    @Ignore
+    public User () {
+    }
+
+    public User (String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
