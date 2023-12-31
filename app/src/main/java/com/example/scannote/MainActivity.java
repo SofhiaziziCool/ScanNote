a5a81c73dca73848112b40eecd79dae6fce44e3f
SofhiaaziziCool <Sofhia33azizi@gmail.com>
2023-12-31 21:26:36 +0800

    initial part of scannote

---
+++ app/src/main/java/com/example/scannote/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.scannote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int notes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
